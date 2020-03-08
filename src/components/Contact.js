97893f4179e6863deaa4d3d78fa28045ee97ae85
Fathul Mundzir <akun.mundzir@gmail.com>
2020-03-08 17:14:48 +0700

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,9 +7,9 @@ export default class Contact extends Component {
       <>
         <h1>NEED MORE TRAFFICS AND SALES REVENUE?</h1>
         <p>
-          Learn how we get traffics through creative content marketing, run and track SEO campaign, and convert it become sales using conversion strategy.
+          Let me tell you how we get traffics through creative content marketing, run and track SEO campaign, and convert it become sales using conversion strategy.
         </p>
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfaGfQx9bPaORQJkIp6snYI70Ht47KAVEZlQwbxadGoqDWhrg/viewform?embedded=true" width="100%" height="2097" frameborder="0" marginheight="0" marginwidth="0" style="margin: 0 !important;">Loading…</iframe>
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfaGfQx9bPaORQJkIp6snYI70Ht47KAVEZlQwbxadGoqDWhrg/viewform?embedded=true" width="100%" height="2097" frameborder="0" marginheight="0" marginwidth="0" style="margin: 0px !important;">Loading…</iframe>
       </>
     )
   }
