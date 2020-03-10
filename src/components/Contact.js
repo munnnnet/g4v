b6f6ea47b338b1391617a4dddb3470047567ed25
Fathul Mundzir <akun.mundzir@gmail.com>
2020-03-10 21:59:52 +0700

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,9 +5,13 @@ export default class Contact extends Component {
   render() {
     return (
       <>
-        <h1>NEED MORE TRAFFICS AND SALES REVENUE?</h1>
+        <h1>THIS IS A REALTIME STATS OF gTAG.DEV METRICS</h1>
         <p>
-          Let me tell you how we get traffics through creative content marketing, run and track SEO campaign, and convert it become sales using conversion strategy.
+          Let me tell you the result of our creative content marketing strategy and SEO campaign tactics.
+        </p>
+        <div style="padding:63% 0 0 0; position:relative;"><iframe src="https://app.databox.com/datawall/c9b9dd653925381ab936bd36d101b47f05e677124?i" style="position:absolute; top:0; left:0; width:100%; height:100%;" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>
+        <p>
+          If you feeling good with this result, tell me what can I do for you and let's make a call conference to discuss that in-depth. 
         </p>
         <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfaGfQx9bPaORQJkIp6snYI70Ht47KAVEZlQwbxadGoqDWhrg/viewform?embedded=true" width="100%" height="571" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
       </>
